f2ae0f146851314e67d27f471acd563995c32fe5
Владислав Чапурин <chapurinvp.18@edu.ystu.ru>
2022-03-21 18:30:34 +0300

    продожение

--- neironki/Program.cs
+++ neironki/Program.cs
@@ -5,16 +5,41 @@ namespace neironki
 {
     class Program
     {
+        public static double[] yFin = new double[2] { 0, 0 };
+        public static int l = 0;
         static void Main(string[] args)
         {
+            begin();            
+    }
+
+        public static void begin()
+        {
             int[] x = insert();
+            double[] y = new double[4];
+            cycl(x);
+            Console.WriteLine("Мин = " + yFin[0] + " Макс = " + yFin[1]);
+        }
+
+        public static void cycl(int[] x)
+        {            
             string[] xdblStr = binar(x);
             double[] y = function(x);
             string[] crsd = cross(xdblStr);
-
+            string[] nextGen = mutation(crsd);
+            int[] nextGenDec = binToDec(nextGen);
+            for (int i = 0; i < 4; i++)
+            {
+                if ((y[i] > yFin[1]) || double.IsNaN(yFin[1])) yFin[1] = y[i];
+                if ((y[i] < yFin[0]) || double.IsNaN(yFin[0])) yFin[0] = y[i];
+            }
+            if (l < 50)
+            {
+                l++;
+                cycl(nextGenDec);
+            }
         }
 
-        static int[] insert()
+        public static int[] insert()
         {
             int[] x = new int[4];
             for (int i = 0; i < x.Length; i++)
@@ -27,7 +52,7 @@ namespace neironki
             return x;
         }
 
-        static string[] binar(int[] x)
+        public static string[] binar(int[] x)
         {           
             int j = x.Length;
             int[] xdbl = new int[j];
@@ -54,7 +79,7 @@ namespace neironki
             return xdblStr;
         }
 
-        static double[] function(int[] x)
+        public static double[] function(int[] x)
         {
             double[] y = new double[x.Length];
             for (int i = 0; i < x.Length; i++)
@@ -66,7 +91,7 @@ namespace neironki
             return y;
         }
 
-        static string[] cross(string[] xdblStr)
+        public static string[] cross(string[] xdblStr)
         {
             string[] xstr = new string[xdblStr.Length];
             int[] x = new int[xdblStr.Length];
@@ -83,6 +108,39 @@ namespace neironki
                 while (xstr[i].Length < 6) xstr[i] = xstr[i].Insert(0, "0");
             }
             return xstr;
+        }
+
+        public static string[] mutation(string[] x)
+        {
+            Random rng = new Random();
+            int NumPos = rng.Next() % x.Length;
+            int BitePos = rng.Next() % 6;
+            if (x[NumPos][BitePos] == '1')
+            {
+                x[NumPos] = x[NumPos].Remove(BitePos, 1);
+                x[NumPos] = x[NumPos].Insert(BitePos, "0");
+            }
+            else
+            {
+                x[NumPos] = x[NumPos].Remove(BitePos, 1);
+                x[NumPos] = x[NumPos].Insert(BitePos, "1");
+            }
+
+            return x;
+        }
+
+        public static int[] binToDec(string[] str)
+        {
+            int[] x = new int[4] { 0, 0, 0, 0 };
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    string v = Convert.ToString(str[j][i]);
+                    x[j] += Convert.ToInt32(v) * Convert.ToInt32(Math.Pow(2, Math.Abs(-5 + i)));
+                }
+            }
+            return x;
         }
         
     }
